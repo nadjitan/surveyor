b08d79006734e6e75502c9fa97ae227ec466d434
nadjitan <nadjitan@yahoo.com.ph>
2022-07-11 22:38:18 +0800

    fix iframe

--- lib/components/Client.tsx
+++ lib/components/Client.tsx
@@ -1,17 +1,172 @@
-import { FC, PropsWithChildren, useState } from "react"
+import { FC, PropsWithChildren, useEffect, useRef, useState } from "react"
 import { DeleteIcon, EditIcon, PlayIcon, SearchIcon, TempIcon } from "./icons"
 import clientStyle from "./Client.module.css"
+
+type Data = { url: string; class: string }
+
+function onClassChange(element: HTMLElement, callback: (node: Node) => void) {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (
+        mutation.type === "attributes" &&
+        mutation.attributeName === "class"
+      ) {
+        callback(mutation.target)
+      }
+    })
+  })
+  observer.observe(element, { attributes: true })
+  return observer.disconnect
+}
 
 /**
  * Import to a dedicated page for ```<iframe />``` to work
  *
  * @returns void
  */
-const Client: FC<PropsWithChildren<{}>> = () => {
+const Client: FC<
+  PropsWithChildren<{ clicks: Data[]; loadIframe: boolean }>
+> = ({ clicks, loadIframe }) => {
   const [url, setUrl] = useState("")
   const [page, setPage] = useState<"data" | "replay">("replay")
 
-  // useEffect(() => setUrl(window.location.origin), [])
+  let recordedElems = useRef<Data[]>([])
+  let timelineElem = useRef<HTMLIFrameElement>(null)
+
+  let iframe: HTMLIFrameElement
+  let iframeDoc: Document
+
+  let prevEl: HTMLElement | null = null
+  let arrIndex = 0
+  let paused = false
+  const delay = 500
+
+  let tlNodes: HTMLCollectionOf<Element>
+  let prevNode: HTMLElement | null = null
+
+  function activateElem() {
+    let elem: HTMLElement
+    iframeDoc.body.querySelectorAll("*").forEach(el => {
+      if (el.classList.contains(clicks[arrIndex].class))
+        elem = el as HTMLElement
+    })
+
+    // TOGGLE STYLES
+    if (prevEl) prevEl.classList.remove("svyr-selected")
+    elem!.classList.add("svyr-selected")
+    if (elem!.tagName !== "A") elem!.click()
+
+    prevEl = elem!
+  }
+
+  function activateNode(index: number, nodes: HTMLCollectionOf<Element>) {
+    if (prevNode) prevNode.classList.remove("svyr-node-selected")
+    nodes[index].classList.add("svyr-node-selected")
+    prevNode = nodes[index] as HTMLElement
+  }
+
+  useEffect(() => {
+    if (page === "replay") {
+      // timelineElem = document.getElementById("timeline") as HTMLIFrameElement
+      // while (timelineElem.current!.firstChild?.nextSibling) {
+      //   timelineElem.current!.removeChild(
+      //     timelineElem.current!.firstChild?.nextSibling
+      //   )
+      // }
+
+      // clicks.forEach((d, index) => {
+      //   const tlItem = document.createElement("div")
+      //   tlItem.classList.add(
+      //     "hover:svyr-bg-theme-primary",
+      //     `svyr-ml-${4}`,
+      //     "svyr-w-4",
+      //     "svyr-h-4",
+      //     "svyr-border-[2px]",
+      //     "svyr-border-theme-grey",
+      //     "svyr-bg-theme-surface",
+      //     "svyr-rotate-45",
+      //     "svyr-cursor-pointer"
+      //   )
+      //   timelineElem.current!.appendChild(tlItem)
+      // })
+
+      if (loadIframe) setUrl(window.location.origin)
+
+      tlNodes = document.getElementsByClassName("svyr-tl-node")
+      Array.from(tlNodes).map((elem, index) => {
+        elem.addEventListener("click", () => {
+          arrIndex = index
+          paused = true
+
+          if (iframe.src !== clicks[arrIndex].url) {
+            iframe.src = clicks[arrIndex].url
+            iframe.onload = e => {
+              iframeDoc = iframeDoc = iframe.contentDocument!
+              activateElem()
+            }
+          } else {
+            activateElem()
+          }
+        })
+      })
+
+      const replayBtn = document.getElementById("btn-replay")
+
+      iframe = document.getElementById("srvyr-website") as HTMLIFrameElement
+
+      iframe.onload = () => {
+        iframeDoc = iframeDoc = iframe.contentDocument!
+
+        // RECORDING
+        iframeDoc.addEventListener("click", e => {
+          const elem = e.target! as HTMLElement
+          const targetClass =
+            elem.className &&
+            Array.from(elem.classList).find(c => c.startsWith("srvyr-"))
+
+          if (targetClass) {
+            recordedElems.current.push({
+              url: iframe.src,
+              class: targetClass,
+            })
+            console.log(recordedElems)
+          }
+        })
+
+        // REPLAY SYSTEM
+        replayBtn!.onclick = () => {
+          // ALWAYS RESET ON CLICK
+          arrIndex = 0
+          prevEl = null
+
+          // LOOPING CLASSES
+          const clicksInterval = setInterval(() => {
+            if (arrIndex < clicks.length) {
+              // IF IFRAME IS NOT EQUAL TO DATA URL
+              if (iframe.src !== clicks[arrIndex].url) {
+                paused = true
+
+                iframe.src = clicks[arrIndex].url
+                iframe.onload = e => {
+                  iframeDoc = iframeDoc = iframe.contentDocument!
+                  onClassChange(iframeDoc.body, () => {
+                    paused = false
+                  })
+                }
+              }
+              if (paused === false) {
+                activateElem()
+                activateNode(arrIndex, tlNodes)
+                arrIndex++
+              }
+            } else {
+              clearInterval(clicksInterval)
+            }
+          }, delay)
+        }
+      }
+    }
+  }, [page])
 
   return (
     <div className={clientStyle.clientBody}>
@@ -96,7 +251,9 @@ const Client: FC<PropsWithChildren<{}>> = () => {
                 </h4>
               </div>
 
-              <button className="svyr-w-28 svyr-rounded-full svyr-text-sm">
+              <button
+                id="btn-replay"
+                className="svyr-w-28 svyr-rounded-full svyr-text-sm">
                 <PlayIcon
                   spanClass="svyr-w-7 svyr-h-full"
                   svgClass="svyr-fill-theme-on-surface svyr-h-5 svyr-w-5"
@@ -106,13 +263,28 @@ const Client: FC<PropsWithChildren<{}>> = () => {
             </div>
 
             <iframe
-              src=""
-              className="svyr-border svyr-w-full svyr-h-full svyr-mt-8"></iframe>
+              id="srvyr-website"
+              src={url}
+              className="svyr-border svyr-w-full svyr-h-full svyr-mt-8 svyr-border-theme-surface"
+            />
 
-            <div className="svyr-bg-theme-surface svyr-h-40 svyr-w-full svyr-overflow-hidden svyr-box-border svyr-p-6 svyr-mt-4">
+            <div className="svyr-bg-theme-surface svyr-h-40 svyr-min-w-full svyr-w-full svyr-relative svyr-overflow-y-hidden svyr-box-border svyr-p-6 svyr-mt-4">
               <h5 className="svyr-text-theme-grey svyr-font-inter-semibold">
                 Timeline
               </h5>
+              <div
+                ref={timelineElem}
+                id="timeline"
+                className="svyr-relative svyr-h-5/6 svyr-overflow-y-hidden svyr-overflow-x-auto svyr-min-w-full svyr-max-w-max svyr-flex svyr-flex-row svyr-items-center">
+                <div className="svyr-h-[2px] svyr-bg-theme-grey svyr-max-w-max svyr-min-w-full svyr-flex svyr-flex-row svyr-items-center svyr-overflow-visible">
+                  {clicks &&
+                    clicks.map(c => (
+                      <div className="svyr-h-max svyr-w-max svyr-ml-4">
+                        <div className="svyr-tl-node hover:svyr-bg-theme-on-surface svyr-w-4 svyr-h-4 svyr-border-[2px] svyr-border-theme-grey svyr-bg-theme-surface svyr-rotate-45 svyr-cursor-pointer" />
+                      </div>
+                    ))}
+                </div>
+              </div>
             </div>
           </>
         )}
@@ -156,8 +328,8 @@ const Client: FC<PropsWithChildren<{}>> = () => {
               </div>
             </div>
 
-            <div className="svyr-w-full svyr-h-16 svyr-justify-center svyr-flex butto-mt-4">
-              <button className="butto-w-4/5 svyr-rounded-full svyr-text-sm">
+            <div className="svyr-w-full svyr-h-16 svyr-justify-center svyr-flex svyr-mt-4">
+              <button className="svyr-w-4/5 svyr-rounded-full svyr-text-sm">
                 <PlayIcon
                   svgClass="svyr-fill-theme-on-surface svyr-h-5 svyr-w-5"
                   spanClass="svyr-w-8 svyr-h-full"
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client"
 import { Client } from "../lib/index"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Client />
-  </React.StrictMode>
+  <Client />
 )
