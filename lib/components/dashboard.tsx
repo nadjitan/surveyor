50376d619c3ceb0f184f9af3d498fa88c2ab6c5d
nadjitan <nadjitan@yahoo.com.ph>
2022-09-20 13:25:17 +0800

    fix ftue

--- lib/components/dashboard.tsx
+++ lib/components/dashboard.tsx
@@ -151,14 +151,8 @@ const Client: FC<{ apiUrl: string }> = ({ apiUrl }) => {
     }
   }, [])
 
-  return page === "recording" ? (
-    <RecordingBody
-      recordedPaths={recordedPaths}
-      setRecordedPaths={setRecordedPaths}
-      setPage={setPage}
-    />
-  ) : (
-    <div className={clientStyle.clientBody}>
+  return (
+    <>
       <dialog
         id="srvyr-help"
         className="svyr-fixed svyr-z-50 svyr-hidden svyr-h-full svyr-w-full svyr-place-items-center svyr-bg-black svyr-bg-opacity-50">
@@ -259,85 +253,99 @@ const Client: FC<{ apiUrl: string }> = ({ apiUrl }) => {
         </div>
       </dialog>
 
-      <nav className={clientStyle.leftNav}>
-        <div onClick={() => setPage("viz")} className={clientStyle.lItem}>
-          <PieChartIcon
-            svgClass={`svyr-w-8 svyr-h-8 ${
-              page !== "viz" && "svyr-stroke-theme-grey"
-            }`}
-          />
-          <span
-            className={`svyr-mt-1 ${page !== "viz" && "svyr-text-theme-grey"}`}>
-            Viz
-          </span>
-        </div>
-        <div onClick={() => setPage("replay")} className={clientStyle.lItem}>
-          <FilmIcon
-            svgClass={`svyr-w-8 svyr-h-8 ${
-              page !== "replay" && "svyr-stroke-theme-grey"
-            }`}
-          />
-          <span
-            className={`svyr-mt-1 ${
-              page !== "replay" && "svyr-text-theme-grey"
-            }`}>
-            Replay
-          </span>
-        </div>
-        <div onClick={() => setPage("data")} className={clientStyle.lItem}>
-          <DatabaseIcon
-            svgClass={`svyr-w-8 svyr-h-8 ${
-              page !== "data" && "svyr-stroke-theme-grey"
-            }`}
-          />
-          <span
-            className={`svyr-mt-1 ${
-              page !== "data" && "svyr-text-theme-grey"
-            }`}>
-            Data
-          </span>
-        </div>
-
-        <div className={clientStyle.lItem}>
-          <HelpIcon
-            title="Help"
-            svgClass="svyr-w-8 svyr-h-8 svyr-stroke-theme-grey"
-            onClick={() => {
-              const dialog = document.getElementById(
-                "srvyr-help"
-              ) as HTMLDialogElement
-
-              dialog.style.display = "grid"
-            }}
-          />
-        </div>
-      </nav>
-
-      {page === "viz" && (
-        <VizBody
-          setPage={setPage}
-          setTelemetryIndex={setTelemetryIndex}
-          selectedRec={selectedRec}
-          mappedTelemetries={mappedTelemetries}
-          setRecordedPaths={setRecordedPaths}
-          setSelectedRec={setSelectedRec}
+      {page === "recording" ? (
+        <RecordingBody
           recordedPaths={recordedPaths}
+          setRecordedPaths={setRecordedPaths}
+          setPage={setPage}
         />
+      ) : (
+        <div className={clientStyle.clientBody}>
+          <nav className={clientStyle.leftNav}>
+            <div onClick={() => setPage("viz")} className={clientStyle.lItem}>
+              <PieChartIcon
+                svgClass={`svyr-w-8 svyr-h-8 ${
+                  page !== "viz" && "svyr-stroke-theme-grey"
+                }`}
+              />
+              <span
+                className={`svyr-mt-1 ${
+                  page !== "viz" && "svyr-text-theme-grey"
+                }`}>
+                Viz
+              </span>
+            </div>
+            <div
+              onClick={() => setPage("replay")}
+              className={clientStyle.lItem}>
+              <FilmIcon
+                svgClass={`svyr-w-8 svyr-h-8 ${
+                  page !== "replay" && "svyr-stroke-theme-grey"
+                }`}
+              />
+              <span
+                className={`svyr-mt-1 ${
+                  page !== "replay" && "svyr-text-theme-grey"
+                }`}>
+                Replay
+              </span>
+            </div>
+            <div onClick={() => setPage("data")} className={clientStyle.lItem}>
+              <DatabaseIcon
+                svgClass={`svyr-w-8 svyr-h-8 ${
+                  page !== "data" && "svyr-stroke-theme-grey"
+                }`}
+              />
+              <span
+                className={`svyr-mt-1 ${
+                  page !== "data" && "svyr-text-theme-grey"
+                }`}>
+                Data
+              </span>
+            </div>
+
+            <div className={clientStyle.lItem}>
+              <HelpIcon
+                title="Help"
+                svgClass="svyr-w-8 svyr-h-8 svyr-stroke-theme-grey"
+                onClick={() => {
+                  const dialog = document.getElementById(
+                    "srvyr-help"
+                  ) as HTMLDialogElement
+
+                  dialog.style.display = "grid"
+                }}
+              />
+            </div>
+          </nav>
+
+          {page === "viz" && (
+            <VizBody
+              setPage={setPage}
+              setTelemetryIndex={setTelemetryIndex}
+              selectedRec={selectedRec}
+              mappedTelemetries={mappedTelemetries}
+              setRecordedPaths={setRecordedPaths}
+              setSelectedRec={setSelectedRec}
+              recordedPaths={recordedPaths}
+            />
+          )}
+          {page === "replay" && (
+            <ReplayBody
+              mappedTelemetries={mappedTelemetries!}
+              telemetryIndex={telemetryIndex}
+            />
+          )}
+          {page === "data" && (
+            <DataBody
+              apiUrl={apiUrl}
+              setMappedTelemetries={setMappedTelemetries}
+              mappedTelemetries={mappedTelemetries!}
+            />
+          )}
+        </div>
       )}
-      {page === "replay" && (
-        <ReplayBody
-          mappedTelemetries={mappedTelemetries!}
-          telemetryIndex={telemetryIndex}
-        />
-      )}
-      {page === "data" && (
-        <DataBody
-          apiUrl={apiUrl}
-          setMappedTelemetries={setMappedTelemetries}
-          mappedTelemetries={mappedTelemetries!}
-        />
-      )}
-    </div>
+    </>
   )
 }
 
